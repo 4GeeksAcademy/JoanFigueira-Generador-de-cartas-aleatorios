b65067f5aa48c2409a3c7fa26a61865846c477ea
Joan Figueira <joanfigueira365@gmail.com>
2024-08-11 17:39:40 +0000

    feat:JoanFigueira/Generador-de-cartas-aleatorias

--- src/app.js
+++ src/app.js
@@ -3,10 +3,8 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
-  // Domain card Generator
+  // Card Generator
   let numbers = [
-    "0",
-    "1",
     "2",
     "3",
     "4",
@@ -16,19 +14,33 @@ window.onload = function() {
     "8",
     "9",
     "10",
-    "11",
-    "12"
+    "J",
+    "Q",
+    "K",
+    "A",
   ];
-  let symbol = ["♦", "♥", "♠", "♣"];
+  let symbols = ["♦", "♥", "♠", "♣"];
 
-  // Metodo .Map()
-  let cardGenerator = "";
-  numbers.map(elementNumbers => {
-    symbol.map(elementSymbol => {
-      cardGenerator += `
-      <div class="col-3">El simbolo ${elementSymbol}, este es el numero ${elementNumbers}</div>
-    `;
-    });
-  });
-  document.querySelector("#card").innerHTML = cardGenerator;
+  // Generar indices aleatorios para cada Array
+  let indexNumbers = Math.floor(Math.random() * numbers.length)
+  let indexSymbols = Math.floor(Math.random() * symbols.length)
+
+  //Template literal
+  let numberGenerator = `
+    ${numbers[indexNumbers]}
+  `
+  let symbolGenerator = `
+    ${symbols[indexSymbols]}
+  `
+
+  //Capturamos div e insertamos en HTML
+  document.querySelector('#number').innerHTML = numberGenerator;
+  document.querySelector('#symboltop').innerHTML = symbolGenerator;
+  document.querySelector('#symbolbutton').innerHTML = symbolGenerator;
+
+  let refresh = document.getElementById('refresh');
+refresh.addEventListener('click', _ => {
+            location.reload();
+})
+  
 };
