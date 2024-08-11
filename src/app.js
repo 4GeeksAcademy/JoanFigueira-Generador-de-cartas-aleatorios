da81d9d934ddc0be3a758955933799bf38fd1056
Joan Figueira <joanfigueira365@gmail.com>
2024-08-11 11:19:10 +0000

    Chau Prettier

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Domain card Generator
+  let numbers = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12"
+  ];
+  let symbol = ["♦", "♥", "♠", "♣"];
+
+  // Metodo .Map()
+  let cardGenerator = "";
+  numbers.map(elementNumbers => {
+    symbol.map(elementSymbol => {
+      cardGenerator += `
+      <div class="col-3">El simbolo ${elementSymbol}, este es el numero ${elementNumbers}</div>
+    `;
+    });
+  });
+  document.querySelector("#card").innerHTML = cardGenerator;
 };
